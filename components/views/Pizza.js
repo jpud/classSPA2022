4f77377cf9654981ad750b6c61641c68bdb8ca8d
Jake Pudiwitr <jakepudiwitr@gmail.com>
2022-03-01 22:03:34 -0600

    SPA PRACTICE

---
+++ components/views/Pizza.js
@@ -0,0 +1,13 @@
+import html from "html-literal";
+export default () => html`
+  <section id="pizza">
+    <table id="pizzas">
+      <tr>
+        <th>Crust</th>
+        <th>Cheese</th>
+        <th>Sauce</th>
+        <th>Toppings</th>
+      </tr>
+    </table>
+  </section>
+`;
